c06b0bc0fcef36891dabd5269f0c567b3454ac9c
kaioken <max@mctekk.com>
2018-12-16 21:50:40 -0400

    update migration

--- storage/db/migrations/20181216170755_subscription_plan_acl.php
+++ storage/db/migrations/20181216170755_subscription_plan_acl.php
@@ -8,8 +8,8 @@ class SubscriptionPlanAcl extends AbstractMigration
     public function change()
     {
         $table = $this->table("users");
-        $table->addColumn('stripe_id', 'string', ['null' => true, 'limit' => 100, 'collation' => "utf8_general_ci", 'encoding' => "utf8", 'after' => 'banned'])->save();
-        $table->addColumn('card_brand', 'string', ['null' => true, 'limit' => 50, 'collation' => "utf8_general_ci", 'encoding' => "utf8", 'after' => 'stripe_id'])->save();
+        $table->addColumn('stripe_id', 'string', ['null' => true, 'limit' => 100, 'collation' => "utf8mb4_unicode_ci", 'encoding' => "utf8mb4", 'after' => 'banned'])->save();
+        $table->addColumn('card_brand', 'string', ['null' => true, 'limit' => 50, 'collation' => "utf8mb4_unicode_ci", 'encoding' => "utf8mb4", 'after' => 'stripe_id'])->save();
         $table->addColumn('card_last_four', 'integer', ['null' => true, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'card_brand'])->save();
         $table->addColumn('trial_ends_at', 'datetime', ['null' => true, 'after' => 'card_last_four'])->save();
 
@@ -20,9 +20,9 @@ class SubscriptionPlanAcl extends AbstractMigration
 
         $table = $this->table("subscriptions");
         $table->addColumn('apps_plans_id', 'integer', ['null' => true, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'apps_id'])->save();
-        $this->table("subscriptions")->changeColumn('name', 'string', ['null' => false, 'limit' => 250, 'collation' => "utf8_unicode_ci", 'encoding' => "utf8", 'after' => 'apps_plans_id'])->update();
-        $this->table("subscriptions")->changeColumn('stripe_id', 'string', ['null' => false, 'limit' => 250, 'collation' => "utf8_unicode_ci", 'encoding' => "utf8", 'after' => 'name'])->update();
-        $this->table("subscriptions")->changeColumn('stripe_plan', 'string', ['null' => false, 'limit' => 250, 'collation' => "utf8_unicode_ci", 'encoding' => "utf8", 'after' => 'stripe_id'])->update();
+        $this->table("subscriptions")->changeColumn('name', 'string', ['null' => false, 'limit' => 250, 'collation' => "utf8mb4_unicode_ci", 'encoding' => "utf8mb4", 'after' => 'apps_plans_id'])->update();
+        $this->table("subscriptions")->changeColumn('stripe_id', 'string', ['null' => false, 'limit' => 250, 'collation' => "utf8mb4_unicode_ci", 'encoding' => "utf8mb4", 'after' => 'name'])->update();
+        $this->table("subscriptions")->changeColumn('stripe_plan', 'string', ['null' => false, 'limit' => 250, 'collation' => "utf8mb4_unicode_ci", 'encoding' => "utf8mb4", 'after' => 'stripe_id'])->update();
         $this->table("subscriptions")->changeColumn('quantity', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'stripe_plan'])->update();
         $this->table("subscriptions")->changeColumn('trial_ends_at', 'timestamp', ['null' => true, 'after' => 'quantity'])->update();
         $this->table("subscriptions")->changeColumn('ends_at', 'timestamp', ['null' => true, 'after' => 'trial_ends_at'])->update();
@@ -47,41 +47,30 @@ class SubscriptionPlanAcl extends AbstractMigration
         $this->table("company_branches")->changeColumn('is_deleted', 'boolean', ['null' => true, 'default' => "0", 'limit' => MysqlAdapter::INT_TINY, 'precision' => 3, 'after' => 'updated_at'])->update();
         $table->save();
 
-        $table = $this->table("system_modules", ['id' => false, 'primary_key' => ["id"], 'engine' => "InnoDB", 'encoding' => "utf8", 'collation' => "utf8_general_mysql500_ci", 'comment' => "list of modules , user can interact on each of the diff apps", 'row_format' => "Dynamic"]);
-        $table->addColumn('id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10])->save();
-        $table->addColumn('name', 'string', ['null' => false, 'limit' => 100, 'collation' => "utf8_general_mysql500_ci", 'encoding' => "utf8", 'after' => 'id'])->save();
-        $table->addColumn('slug', 'string', ['null' => false, 'limit' => 100, 'collation' => "utf8_general_mysql500_ci", 'encoding' => "utf8", 'after' => 'name'])->save();
-        $table->addColumn('model_name', 'string', ['null' => false, 'limit' => 100, 'collation' => "utf8_general_mysql500_ci", 'encoding' => "utf8", 'after' => 'slug'])->save();
-        $table->addColumn('apps_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'model_name'])->save();
-        $table->addColumn('parents_id', 'integer', ['null' => true, 'default' => "0", 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'apps_id'])->save();
-        $table->addColumn('menu_order', 'integer', ['null' => true, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'parents_id'])->save();
-        $table->addColumn('created_at', 'datetime', ['null' => false, 'after' => 'menu_order'])->save();
-        $table->addColumn('updated_at', 'datetime', ['null' => true, 'after' => 'created_at'])->save();
-        $table->addColumn('is_deleted', 'boolean', ['null' => true, 'default' => "0", 'limit' => MysqlAdapter::INT_TINY, 'precision' => 3, 'after' => 'updated_at'])->save();
-        $table->save();
+        $table = $this->table("system_modules", ['id' => false, 'primary_key' => ["id"], 'engine' => "InnoDB", 'encoding' => "utf8mb4", 'collation' => "utf8mb4_unicode_ci", 'comment' => "list of modules , user can interact on each of the diff apps", 'row_format' => "Dynamic"]);
+        $table->addColumn('id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10])
+            ->addColumn('name', 'string', ['null' => false, 'limit' => 100, 'collation' => "utf8mb4_unicode_ci", 'encoding' => "utf8mb4", 'after' => 'id'])
+            ->addColumn('slug', 'string', ['null' => false, 'limit' => 100, 'collation' => "utf8mb4_unicode_ci", 'encoding' => "utf8mb4", 'after' => 'name'])
+            ->addColumn('model_name', 'string', ['null' => false, 'limit' => 100, 'collation' => "utf8mb4_unicode_ci", 'encoding' => "utf8mb4", 'after' => 'slug'])
+            ->addColumn('apps_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'model_name'])
+            ->addColumn('parents_id', 'integer', ['null' => true, 'default' => "0", 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'apps_id'])
+            ->addColumn('menu_order', 'integer', ['null' => true, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'parents_id'])
+            ->addColumn('created_at', 'datetime', ['null' => false, 'after' => 'menu_order'])
+            ->addColumn('updated_at', 'datetime', ['null' => true, 'after' => 'created_at'])
+            ->addColumn('is_deleted', 'boolean', ['null' => true, 'default' => "0", 'limit' => MysqlAdapter::INT_TINY, 'precision' => 3, 'after' => 'updated_at'])
+            ->save();
 
-        $table = $this->table("user_company_apps_activities", ['id' => false, 'primary_key' => ["company_id", "company_branches_id", "apps_id", "key"], 'engine' => "InnoDB", 'encoding' => "utf8", 'collation' => "utf8_general_mysql500_ci", 'comment' => "what are the uses doing in the current app they are using?", 'row_format' => "Dynamic"]);
-        $table->addColumn('company_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10])->save();
-        $table->addColumn('company_branches_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'company_id'])->save();
-        $table->addColumn('apps_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'company_branches_id'])->save();
-        $table->addColumn('key', 'string', ['null' => false, 'limit' => 50, 'collation' => "utf8_general_mysql500_ci", 'encoding' => "utf8", 'after' => 'apps_id'])->save();
-        $table->addColumn('value', 'string', ['null' => false, 'limit' => 255, 'collation' => "utf8_general_mysql500_ci", 'encoding' => "utf8", 'after' => 'key'])->save();
-        $table->addColumn('created_at', 'datetime', ['null' => false, 'after' => 'value'])->save();
-        $table->addColumn('updated_at', 'datetime', ['null' => true, 'after' => 'created_at'])->save();
-        $table->addColumn('is_deleted', 'boolean', ['null' => true, 'default' => "0", 'limit' => MysqlAdapter::INT_TINY, 'precision' => 3, 'after' => 'updated_at'])->save();
-        $table->save();
+        $table = $this->table("user_company_apps_activities", ['id' => false, 'primary_key' => ["company_id", "company_branches_id", "apps_id", "key"], 'engine' => "InnoDB", 'encoding' => "utf8mb4", 'collation' => "utf8mb4_unicode_ci", 'comment' => "what are the uses doing in the current app they are using?", 'row_format' => "Dynamic"]);
+        $table->addColumn('company_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10])
+            ->addColumn('company_branches_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'company_id'])
+            ->addColumn('apps_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'company_branches_id'])
+            ->addColumn('key', 'string', ['null' => false, 'limit' => 50, 'collation' => "utf8mb4_unicode_ci", 'encoding' => "utf8mb4", 'after' => 'apps_id'])
+            ->addColumn('value', 'string', ['null' => false, 'limit' => 255, 'collation' => "utf8mb4_unicode_ci", 'encoding' => "utf8mb4", 'after' => 'key'])
+            ->addColumn('created_at', 'datetime', ['null' => false, 'after' => 'value'])
+            ->addColumn('updated_at', 'datetime', ['null' => true, 'after' => 'created_at'])
+            ->addColumn('is_deleted', 'boolean', ['null' => true, 'default' => "0", 'limit' => MysqlAdapter::INT_TINY, 'precision' => 3, 'after' => 'updated_at'])
+            ->save();
 
-        $table = $this->table("email_templates", ['id' => false, 'primary_key' => ["id"], 'engine' => "InnoDB", 'encoding' => "utf8mb4", 'collation' => "utf8mb4_unicode_ci", 'comment' => "", 'row_format' => "Dynamic"]);
-        $table->addColumn('id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'identity' => 'enable'])->save();
-        $table->addColumn('users_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'id'])->save();
-        $table->addColumn('company_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'users_id'])->save();
-        $table->addColumn('app_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'company_id'])->save();
-        $table->addColumn('name', 'string', ['null' => false, 'limit' => 64, 'collation' => "utf8mb4_unicode_ci", 'encoding' => "utf8mb4", 'after' => 'app_id'])->save();
-        $table->addColumn('template', 'text', ['null' => false, 'limit' => MysqlAdapter::TEXT_LONG, 'collation' => "utf8mb4_unicode_ci", 'encoding' => "utf8mb4", 'after' => 'name'])->save();
-        $table->addColumn('created_at', 'datetime', ['null' => false, 'after' => 'template'])->save();
-        $table->addColumn('updated_at', 'datetime', ['null' => true, 'after' => 'created_at'])->save();
-        $table->addColumn('is_deleted', 'integer', ['null' => false, 'default' => "0", 'limit' => MysqlAdapter::INT_TINY, 'precision' => 3, 'after' => 'updated_at'])->save();
-        $table->save();
 
         $table = $this->table("email_templates");
         if ($table->hasIndex("company_id_app_id_name")) {
@@ -90,26 +79,6 @@ class SubscriptionPlanAcl extends AbstractMigration
 
         $table = $this->table("email_templates");
         $table->addIndex(['company_id', 'app_id', 'name'], ['name' => "company_id_app_id_name", 'unique' => true])->save();
-        $table = $this->table("users_invite", ['id' => false, 'primary_key' => ["id"], 'engine' => "InnoDB", 'encoding' => "utf8mb4", 'collation' => "utf8mb4_unicode_ci", 'comment' => "", 'row_format' => "Dynamic"]);
-        $table->addColumn('id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'identity' => 'enable'])->save();
-        $table->addColumn('invite_hash', 'string', ['null' => false, 'limit' => 200, 'collation' => "utf8mb4_unicode_ci", 'encoding' => "utf8mb4", 'after' => 'id'])->save();
-        $table->addColumn('company_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'invite_hash'])->save();
-        $table->addColumn('role_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'company_id'])->save();
-        $table->addColumn('app_id', 'integer', ['null' => false, 'limit' => MysqlAdapter::INT_REGULAR, 'precision' => 10, 'after' => 'role_id'])->save();
-        $table->addColumn('email', 'string', ['null' => false, 'limit' => 64, 'collation' => "utf8mb4_unicode_ci", 'encoding' => "utf8mb4", 'after' => 'app_id'])->save();
-        $table->addColumn('created_at', 'datetime', ['null' => false, 'after' => 'email'])->save();
-        $table->addColumn('updated_at', 'datetime', ['null' => true, 'after' => 'created_at'])->save();
-        $table->addColumn('is_deleted', 'integer', ['null' => false, 'default' => "0", 'limit' => MysqlAdapter::INT_TINY, 'precision' => 3, 'after' => 'updated_at'])->save();
-        $table->save();
 
-        $table = $this->table("suscriptions");
-        if ($table->hasIndex("PRIMARY")) {
-            $table->removeIndexByName("PRIMARY")->save();
-        }
-        $table = $this->table("suscriptions");
-        if ($table->hasIndex("company_id")) {
-            $table->removeIndexByName("company_id")->save();
-        }
-        $this->table("suscriptions")->drop()->save();
     }
 }
