32ca7b896109185670a6ba49249bf3b0bfc3ce7d
Rafael White <rwhite@mctekk.com>
2019-03-07 12:01:29 -0400

    Replace exception

--- library/Providers/DatabaseProvider.php
+++ library/Providers/DatabaseProvider.php
@@ -8,8 +8,8 @@ use function Gewaer\Core\envValue;
 use Phalcon\Db\Adapter\Pdo\Mysql;
 use Phalcon\Di\ServiceProviderInterface;
 use Phalcon\DiInterface;
-use Exception;
 use PDOException;
+use Gewaer\Exception\ServerErrorHttpException;
 
 class DatabaseProvider implements ServiceProviderInterface
 {
@@ -35,7 +35,7 @@ class DatabaseProvider implements ServiceProviderInterface
                     // Set everything to UTF8
                     $connection->execute('SET NAMES utf8mb4', []);
                 } catch (PDOException $e) {
-                    throw new Exception($e->getMessage());
+                    throw new ServerErrorHttpException($e->getMessage());
                 }
 
                 return $connection;
