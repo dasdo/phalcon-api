deee20bce8b2674b475a428908f4420a2c47e8f5
mctekk <max@mctekk.com>
2019-04-19 19:59:09 -0400

    remvoe delete reference

--- library/Http/Request.php
+++ library/Http/Request.php
@@ -5,9 +5,6 @@ declare(strict_types=1);
 namespace Gewaer\Http;
 
 use Phalcon\Http\Request as PhRequest;
-use Baka\Http\RouterCollection;
-use Phalcon\Mvc\Router\Route;
-use Gewaer\Contracts\RequestJwtInterface;
 use Gewaer\Traits\RequestJwtTrait;
 
 class Request extends PhRequest
