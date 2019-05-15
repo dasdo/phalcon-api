6ade564a504a6f6d85b4aea4819176366ce4038f
Rafael White <rwhite@mctekk.com>
2019-05-15 16:17:41 -0400

    Add test for user email change

--- tests/api/AuthCest.php
+++ tests/api/AuthCest.php
@@ -85,4 +85,49 @@ class AuthCest
         $I->assertTrue(isset($data['id']));
         $I->assertTrue(isset($data['token']));
     }
+
+    /**
+     * Change user's email and relogin
+     * @param ApiTester $I
+     * @return void
+     */
+    public function changeUserEmail(ApiTester $I)
+    {
+        $userData = $I->apiLogin();
+        $newEmail =  'tes3t@baka.io';
+
+        //Get current user info
+        $I->haveHttpHeader('Authorization', $userData->token);
+        $I->sendGet('/v1/users/' . $userData->id);
+
+        $I->seeResponseIsSuccessful();
+        $response = $I->grabResponse();
+        $user = json_decode($response, true);
+
+        //Try to change user's email
+        $I->haveHttpHeader('Authorization', $userData->token);
+        $I->sendPost('/v1/users/' . $user['user_activation_email'] . '/reset-key', [
+            'new_email' => $newEmail,
+            'password'=>Data::loginJson()['password']
+        ]);
+
+        $I->seeResponseIsSuccessful();
+        $response = $I->grabResponse();
+        $data = json_decode($response, true);
+
+        $I->assertTrue($data['id'] == $user['id']);
+
+        // //Revert to old email
+        $I->haveHttpHeader('Authorization', $data['token']);
+        $I->sendPost('/v1/users/' . $user['user_activation_email'] . '/reset-key', [
+            'new_email' => 'tes2t@baka.io',
+            'password'=>Data::loginJson()['password']
+        ]);
+
+        $I->seeResponseIsSuccessful();
+        $response = $I->grabResponse();
+        $newData = json_decode($response, true);
+
+        $I->assertTrue($newData['id'] == $data['id']);
+    }
 }
