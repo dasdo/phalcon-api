82d8dbf33797c5912b0b2eef3eb88a19586a19e8
mctekk <max@mctekk.com>
2019-06-13 10:27:56 -0400

    fix config provider dependency, moved from canvas core to gewaer provider

--- tests/integration/library/Models/RolesCest.php
+++ tests/integration/library/Models/RolesCest.php
@@ -4,7 +4,7 @@ namespace Gewaer\Tests\integration\library\Models;
 
 use Canvas\Models\Apps;
 use IntegrationTester;
-use Canvas\Providers\ConfigProvider;
+use Gewaer\Providers\ConfigProvider;
 use Phalcon\Di\FactoryDefault;
 use Canvas\Models\Roles;
 use Canvas\Models\Companies;
